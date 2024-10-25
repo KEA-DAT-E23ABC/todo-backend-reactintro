0c0aec530b0c41b049ffe4b5f18c57e15e139ede
osman-butt <butt.osman@gmail.com>
2024-10-25 02:15:22 +0200

    Add CRUD endpoints

---
+++ src/test/java/kea/reactintro/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.reactintro.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
